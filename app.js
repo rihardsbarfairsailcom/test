44c491fd6a18d0acb5a41ca727d2ffc522ea2fa1
Rihards Baranovskis <rihards.baranovskis@sage.com>
2017-10-02 17:57:17 +0100

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World this is dawg2')
+})
+
+app.listen(3000, function () {
+  console.log('port 3000!')
+})
