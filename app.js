53235c8c15ecc2abaa174f19e7a87814bf8f3e7f
Rihards Baranovskis <rihards.baranovskis@sage.com>
2017-10-24 12:03:43 +0100

    local change 1.3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World this is dawg232')
+  res.send('Hello World this is dawg2322')
 })
 
 app.listen(3000, function () {
